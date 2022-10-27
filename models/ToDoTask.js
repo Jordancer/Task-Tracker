97eb287038da02dc4b578825c25b77798e8d4631
Jordan Dancer <112099523+Jordancer@users.noreply.github.com>
2022-10-27 09:50:30 -0400

    Fin

--- models/ToDoTask.js
+++ models/ToDoTask.js
@@ -1,5 +1,9 @@
 // Make collection schema and export so it can be used on index.js file.
+// data structures that we use to define the shape of our data
+// front end to MongoDB
+
 const mongoose = require("mongoose");
+
 const todoTaskSchema = new mongoose.Schema({
   content: {
     type: String,
